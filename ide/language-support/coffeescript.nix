53490e43e8ae8604f07cb9ac751f69392f1e548d
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-08-27 18:18:30 -0400

    Fixed a small error

--- ide/language-support/coffeescript.nix
+++ ide/language-support/coffeescript.nix
@@ -1,19 +1,28 @@
-{ pkgs, lib, config, ... }:
-
 {
-  options.programs.emacs.init.ide.languages.coffeescript.enable = lib.mkEnableOption "Enables coffeescript support.";
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+let
+  ide = config.programs.emacs.init.ide;
+in
+{
+  options.programs.emacs.init.ide.languages.coffeescript.enable =
+    lib.mkEnableOption "Enables coffeescript support.";
 
   config = lib.mkIf config.programs.emacs.init.ide.languages.coffeescript.enable {
     programs.emacs.init.usePackage = {
       coffee-mode = {
         enable = true;
-        hook = ["(coffee-mode . coffee-cos-mode)"];
+        hook = [ "(coffee-mode . coffee-cos-mode)" ];
       };
 
       flymake-coffee = lib.mkIf ide.flymake.enable {
         enable = true;
-        extraPackages = [pkgs.coffeescript];
-        hook = ["(coffee-mode . flymake-coffee-load)"];
+        extraPackages = [ pkgs.coffeescript ];
+        hook = [ "(coffee-mode . flymake-coffee-load)" ];
       };
 
       ob-coffeescript = lib.mkIf ide.languages.org.enable {
