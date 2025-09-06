7dac2d216a4aa71f7076a2492a8dcf94aebab4b3
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-09-06 12:15:45 -0400

    lsp-mode now supports scheme

--- ide/language-support/scheme.nix
+++ ide/language-support/scheme.nix
@@ -17,13 +17,25 @@ in
     programs.emacs.init.usePackage = {
       scheme = {
         enable = true;
-        extraPackages = [ pkgs.akkuPackages.scheme-langserver ];
+        extraPackages = lib.mkIf (ide.eglot.enable || ide.lsp.enable || ide.lspce.enable) [ pkgs.akkuPackages.scheme-langserver ];
         babel = lib.mkIf ide.languages.org.enable "scheme";
         symex = ide.symex;
         eglot = lib.mkIf ide.eglot.enable '''(scheme-mode . ("scheme-langserver"))'';
         lspce = lib.mkIf ide.lspce.enable ''"scheme" "scheme-langserver"'';
+        lsp = lib.mkIf ide.lsp.enable;
         init = ''(setq auto-mode-alist (delete '("\\.rkt\\'" . scheme-mode) auto-mode-alist))'';
-        config = ''(setq auto-mode-alist (delete '("\\.rkt\\'" . scheme-mode) auto-mode-alist))'';
+        config = ''
+          (setq auto-mode-alist (delete '("\\.rkt\\'" . scheme-mode) auto-mode-alist))
+          ${if ide.lsp.enable then ''
+          (with-eval-after-load 'lsp-mode
+                                (add-to-list 'lsp-language-id-configuration '(scheme-mode . "scheme"))
+                                (lsp-register-client
+                                  (make-lsp-client :new-connection (lsp-stdio-connection "scheme-langserver")
+                                                                                     :major-modes '(scheme-mode)
+                                                   :activation-fn (lsp-activate-on "scheme")
+                                                   :server-id 'scheme-langserver)))
+        '' else ""}
+        '';
       };
     };
   };
