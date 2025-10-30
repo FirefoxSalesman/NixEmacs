ec1a370d82085d7cb900a50e2ca37822a65014a0
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-10-30 10:42:49 -0400

    Renamed emacsPackages to emacs.pkgs

--- overlay.nix
+++ overlay.nix
@@ -4,21 +4,21 @@ final: prev: inputs: {
     ((prev.emacsPackagesFor emacs).overrideScope (
       nfinal: nprev: {
         org-modern-indent = (
-          prev.emacsPackages.callPackage ./emacs-packages/org-modern-indent.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/org-modern-indent.nix {
             inherit inputs;
-            inherit (prev.emacsPackages) trivialBuild compat;
+            inherit (prev.emacs.pkgs) trivialBuild compat;
           }
         );
         doom-utils = (
-          prev.emacsPackages.callPackage ./emacs-packages/doom-utils.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/doom-utils.nix {
             inherit inputs;
-            inherit (prev.emacsPackages) trivialBuild;
+            inherit (prev.emacs.pkgs) trivialBuild;
           }
         );
         eglot-x = (
-          prev.emacsPackages.callPackage ./emacs-packages/eglot-x.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/eglot-x.nix {
             inherit inputs;
-            inherit (prev.emacsPackages)
+            inherit (prev.emacs.pkgs)
               trivialBuild
               eglot
               project
@@ -27,9 +27,9 @@ final: prev: inputs: {
           }
         );
         symex = (
-          prev.emacsPackages.callPackage ./emacs-packages/symex.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/symex.nix {
             inherit inputs;
-            inherit (prev.emacsPackages)
+            inherit (prev.emacs.pkgs)
               trivialBuild
               seq
               mantra
@@ -41,27 +41,27 @@ final: prev: inputs: {
           }
         );
         eglot-booster = (
-          prev.emacsPackages.callPackage ./emacs-packages/eglot-booster.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/eglot-booster.nix {
             inherit inputs;
-            inherit (prev.emacsPackages) trivialBuild eglot jsonrpc;
+            inherit (prev.emacs.pkgs) trivialBuild eglot jsonrpc;
           }
         );
         svelte-ts-mode = (
-          prev.emacsPackages.callPackage ./emacs-packages/svelte-ts-mode.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/svelte-ts-mode.nix {
             inherit inputs;
-            inherit (prev.emacsPackages) trivialBuild;
+            inherit (prev.emacs.pkgs) trivialBuild;
           }
         );
         use-package-eglot = (
-          prev.emacsPackages.callPackage ./emacs-packages/use-package-eglot.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/use-package-eglot.nix {
             inherit inputs;
-            inherit (prev.emacsPackages) trivialBuild use-package eglot;
+            inherit (prev.emacs.pkgs) trivialBuild use-package eglot;
           }
         );
         org-novelist = (
-          prev.emacsPackages.callPackage ./emacs-packages/org-novelist.nix {
+          prev.emacs.pkgs.callPackage ./emacs-packages/org-novelist.nix {
             inherit inputs;
-            inherit (prev.emacsPackages) trivialBuild org;
+            inherit (prev.emacs.pkgs) trivialBuild org;
           }
         );
       }
