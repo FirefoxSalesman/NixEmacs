f8cf2807241f53aac1c8f5d979711a0076332375
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-11-05 19:04:52 -0500

    Jupyter support

--- ide/language-support/python.nix
+++ ide/language-support/python.nix
@@ -13,7 +13,7 @@ in
 {
   options.programs.emacs.init.ide.languages.python = {
     enable = lib.mkEnableOption "Enables python support. Partly borrowed from snakemacs.";
-    jupyter = lib.mkEnableOption "enables code-cells, a minor mode for editing jupyter files";
+    jupyter = lib.mkEnableOption "enables code-cells, a minor mode for editing jupyter files. If org support is enabled, jupyter kernels can be calledfrom org-babel";
     snakemake = lib.mkEnableOption "enables snakemake support";
     languageServer = lib.mkOption {
       type = lib.types.str;
@@ -111,6 +111,11 @@ in
         hook = [ "(python-ts-mode . pet-mode)" ];
       };
 
+      snakemake-mode = lib.mkIf ide.languages.python.snakemake {
+        enable = true;
+        mode = [ ''"Snakefile\\'"'' ];
+      };
+
       code-cells = lib.mkIf ide.languages.python.jupyter {
         enable = true;
         demand = lib.mkDefault true;
@@ -124,6 +129,66 @@ in
             "M-${keybinds.evil.keys.up}" = "'code-cells-backward-cell";
           };
         };
+      };
+
+      # Entirely stolen from snakemacs. The free functions are much appreciated
+      jupyter = lib.mkIf (ide.languages.python.jupyter && ide.languages.org.enable) {
+        enable = true;
+        after = [ "org" ];
+        babel = "jupyter";
+        extraPackages = [ pkgs.jupyter ];
+        hook = [ "(org-after-execute . org-redisplay-inline-images)" ];
+        preface = ''
+          (defun nix-emacs/jupyter-refresh-kernelspecs ()
+                 "Refresh Jupyter kernelspecs"
+                 (interactive)
+                 (jupyter-available-kernelspecs t))
+
+          (defun nix-emacs/jupyter-refesh-langs ()
+                 "Refresh Jupyter languages"
+                 (interactive)
+                 (org-babel-jupyter-aliases-from-kernelspecs t))
+          (defvar nix-emacs/jupyter-runtime-folder (expand-file-name "~/.local/share/jupyter/runtime"))
+          (defun nix-emacs/get-open-ports ()
+                 (mapcar #'string-to-number
+                         (split-string (shell-command-to-string
+                                        "ss -tulpnH | awk '{print $5}' | sed -e 's/.*://'")
+                                        "\n")))
+          (defun nix-emacs/list-jupyter-kernel-files ()
+                 (mapcar
+                  (lambda (file)
+                          (cons
+                           (car file)
+                           (cdr (assq 'shell_port (json-read-file (car file))))))
+                  (sort (directory-files-and-attributes nix-emacs/jupyter-runtime-folder
+                                         t ".*kernel.*json$")
+                        (lambda (x y) (not (time-less-p (nth 6 x) (nth 6 y)))))))
+          (defun nix-emacs/select-jupyter-kernel ()
+                 (let ((ports (nix-emacs/get-open-ports))
+                       (files (nix-emacs/list-jupyter-kernel-files)))
+                      (completing-read "Jupyter kernels: "
+                        (seq-filter (lambda (file) (member (cdr file) ports)) files))))
+          (defun nix-emacs/insert-jupyter-kernel ()
+                 "Insert a path to an active Jupyter kernel into the buffer"
+                 (interactive)
+                 (insert (nix-emacs/select-jupyter-kernel)))
+          (defun nix-emacs/jupyter-connect-repl ()
+                 "Open an emacs-jupyter REPL, connected to a Jupyter kernel"
+                 (interactive)
+                 (jupyter-connect-repl (nix-emacs/select-jupyter-kernel) nil nil nil t))
+          (defun nix-emacs/jupyter-cleanup-kernels ()
+                 (interactive)
+                 (let* ((ports (nix-emacs/get-open-ports))
+                        (files (nix-emacs/list-jupyter-kernel-files))
+                        (to-delete
+                         (seq-filter
+                          (lambda (file) (not (member (cdr file) ports))) files)))
+                       (when (and (length> to-delete 0)
+                                  (y-or-n-p
+                                   (format "Delete %d files?" (length to-delete))))
+                             (dolist (file to-delete)
+                                     (delete-file (car file))))))
+        '';
       };
     };
   };
