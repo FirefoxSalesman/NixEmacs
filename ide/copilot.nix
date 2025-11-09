75a2a371696c7e8645b3822a832c60e8b2a7ec32
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-11-09 12:47:31 -0500

    Copilot will stop bothering the user after all relevant buffers are killed

--- ide/copilot.nix
+++ ide/copilot.nix
@@ -28,6 +28,29 @@ in
         	                       (copilot-mode))))
       ''
     ];
+    preface = ''
+      (defun nix-emacs/kill-copilot ()
+        "Kill copilot process & related buffers."
+        (interactive)  
+        (jsonrpc-shutdown copilot--connection)
+        (dolist (buf '("*copilot-language-server-log*" "*copilot events*"))
+          (when (get-buffer buf)
+            (kill-buffer buf))))
+    '';
+    config = ''
+            (add-hook 'kill-buffer-hook
+                      (lambda ()
+                              (when (and (not (or (equal "*copilot-language-server-log*" (buffer-name))
+                                                  (equal "*copilot events*" (buffer-name))))
+                                         (length= (-filter (lambda (x)
+                                                                   (with-current-buffer x
+                                                                                        (not (or (eq (derived-mode-p 'prog-mode) nil)
+      										                 ${lib.concatMapStrings (k: "(major-mode? ${k})\n") ide.copilot.keepOutOf}
+                                                                                                 (s-contains? "org-src-fontification" (buffer-name))))))
+            			                           (buffer-list))
+            		                          0))
+            	                    (nix-emacs/kill-copilot))))
+    '';
     bindLocal.copilot-completion-map = {
       "TAB" = lib.mkDefault "copilot-accept-completion";
       "<tab>" = lib.mkDefault "copilot-accept-completion";
