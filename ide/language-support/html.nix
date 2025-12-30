623223ae4720b260811355b9166c215c0c5284d1
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-12-30 09:17:29 -0500

    More bugfixing

--- ide/language-support/html.nix
+++ ide/language-support/html.nix
@@ -15,30 +15,33 @@ in
   };
 
   config = lib.mkIf ide.languages.html.enable {
-    programs.emacs.init.usePackage = {
+    programs.emacs.init = {
       ide = {
         treesitter.wantTreesitter = true;
         treesit-fold.enabledModes = lib.mkIf ide.treesit-fold.enable [ "html-ts-mode" ];
       };
-      html-ts-mode = {
-        enable = true;
-        extraPackages =
-          if ide.lsp-bridge.enable || ide.lsp.enable || ide.eglot.enable then
-            with pkgs; [ vscode-langservers-extracted ]
-          else
-            [ ];
-        # many thanks to doom
-        mode = [ ''"\\.[px]?html?\\'"'' ];
-        lspce = lib.mkIf ide.lspce.enable ''"html" "vscode-html-language-server" "--stdio"'';
-        eglot = lib.mkIf ide.eglot.enable ''("vscode-html-language-server" "--stdio")'';
-        symex = ide.symex;
-        lsp = ide.lsp.enable;
-      };
 
-      emmet-mode = {
-        enable = ide.languages.html.emmet;
-        hook = [ "(html-ts-mode . emmet-mode)" ];
-        setopt.emmet-move-cursor-between-quotes = lib.mkDefault true;
+      usePackage = {
+        html-ts-mode = {
+          enable = true;
+          extraPackages =
+            if ide.lsp-bridge.enable || ide.lsp.enable || ide.eglot.enable then
+              with pkgs; [ vscode-langservers-extracted ]
+            else
+              [ ];
+          # many thanks to doom
+          mode = [ ''"\\.[px]?html?\\'"'' ];
+          lspce = lib.mkIf ide.lspce.enable ''"html" "vscode-html-language-server" "--stdio"'';
+          eglot = lib.mkIf ide.eglot.enable ''("vscode-html-language-server" "--stdio")'';
+          symex = ide.symex;
+          lsp = ide.lsp.enable;
+        };
+
+        emmet-mode = {
+          enable = ide.languages.html.emmet;
+          hook = [ "(html-ts-mode . emmet-mode)" ];
+          setopt.emmet-move-cursor-between-quotes = lib.mkDefault true;
+        };
       };
     };
   };
