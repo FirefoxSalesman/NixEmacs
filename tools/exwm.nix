40b2a6890acfa676afcfd7157f62ddd7820738eb
Carson Holscher <firefoxsalesman2@protonmail.com>
2026-03-17 19:56:38 -0400

    Exwm buffers are automatically put in motion state

--- tools/exwm.nix
+++ tools/exwm.nix
@@ -3,23 +3,17 @@
 let
   makeBinds = vs: lib.optionals (vs != { }) (lib.mapAttrsToList (n: v: "`([${n}] . ${v})") vs);
   tools = config.programs.emacs.init.tools;
-  bindType =
-    desc:
-    lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      default = { };
-      description =
-        desc
-        + " Exported in the form ([key] . command). These items are syntax quoted for your convenience. In order to use a modifier key, you must prefix the modifier with \\?";
-    };
 in
 {
   options.programs.emacs.init.tools.exwm = {
     enable = lib.mkEnableOption "Enables exwm";
     # useGaps = lib.mkEnableOption "Enables gaps around the emacs frame";
     wantMouseWarping = lib.mkEnableOption "Enables mouse warping to the center of the focused window";
-    bindings = bindType "Keybindings for exwm. Left hand side is the key, right hand side is the command.";
-    simulationKeys = bindType "Simulation keys for exwm. Left hand side is the original key, right hand side is the new key.";
+    bindings = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = { };
+      description = "Keybindings for exwm. Left hand side is the key, right hand side is the command. Exported in the form ([key] . command). These items are syntax quoted for your convenience. In order to use a modifier key, you must prefix the modifier with \\?";
+    };
   };
 
   config.programs.emacs.init = lib.mkIf tools.exwm.enable {
@@ -28,12 +22,17 @@ in
       exwm = {
         enable = true;
         afterCall = [ "on-init-ui-hook" ];
+        hook =
+          [ ]
+          ++ (
+            if config.programs.emacs.init.keybinds.evil.enable then
+              [ "(exwm-mode . evil-motion-state)" ]
+            else
+              [ ]
+          );
         custom = {
           exwm-workspace-warp-cursor = lib.mkDefault tools.exwm.wantMouseWarping;
-          exwm-input-global-keys = lib.mkIf (tools.exwm.bindings != { }) (makeBinds tools.exwm.bindings);
-          exwm-input-simulation-keys = lib.mkIf (tools.exwm.bindings != { }) (
-            makeBinds tools.exwm.simulationKeys
-          );
+          exwm-input-global-keys = makeBinds tools.exwm.bindings;
         };
       };
 
