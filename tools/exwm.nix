f5b9bbfc9c531dfed8b91895582bbc4230738d55
Carson Holscher <firefoxsalesman2@protonmail.com>
2026-03-17 19:20:18 -0400

    Brought back bindings

--- tools/exwm.nix
+++ tools/exwm.nix
@@ -1,24 +1,24 @@
 { lib, config, ... }:
 
 let
-  # makeBinds = vs: lib.optionals (vs != { }) (lib.mapAttrsToList (n: v: "`([?\${n}] . ${v})"));
+  makeBinds = vs: lib.optionals (vs != { }) (lib.mapAttrsToList (n: v: "`([?\${n}] . ${v})"));
   tools = config.programs.emacs.init.tools;
-  # bindType =
-  #   desc:
-  #   lib.mkOption {
-  #     type = lib.types.attrsOf lib.types.str;
-  #     default = { };
-  #     description =
-  #       desc
-  #       + " Exported in the form ([?\\key] . command). These items are syntax quoted for your convenience.";
-  #   };
+  bindType =
+    desc:
+    lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = { };
+      description =
+        desc
+        + " Exported in the form ([?\\key] . command). These items are syntax quoted for your convenience.";
+    };
 in
 {
   options.programs.emacs.init.tools.exwm = {
     enable = lib.mkEnableOption "Enables exwm";
     # useGaps = lib.mkEnableOption "Enables gaps around the emacs frame";
     wantMouseWarping = lib.mkEnableOption "Enables mouse warping to the center of the focused window";
-    # bindings = bindType "Keybindings for exwm. Left hand side is the key, right hand side is the command.";
+    bindings = bindType "Keybindings for exwm. Left hand side is the key, right hand side is the command.";
     # simulationKeys = bindType "Simulation keys for exwm. Left hand side is the original key, right hand side is the new key.";
   };
 
@@ -30,7 +30,7 @@ in
         afterCall = [ "on-init-ui-hook" ];
         custom = {
           exwm-workspace-warp-cursor = lib.mkDefault tools.exwm.wantMouseWarping;
-          # exwm-input-global-keys = makeBinds tools.exwm.bindings;
+          exwm-input-global-keys = makeBinds tools.exwm.bindings;
           # exwm-input-simulation-keys = makeBinds tools.exwm.simulationKeys;
         };
       };
