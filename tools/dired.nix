c5faeb6138d5cfd7c1e9512a132a4000ea16fe57
Carson Holscher <firefoxsalesman2@protonmail.com>
2026-04-13 00:47:12 -0400

    Added async option to dired

--- tools/dired.nix
+++ tools/dired.nix
@@ -9,14 +9,17 @@ in
     enable = lib.mkEnableOption "Enable dired improvements.";
     narrow = lib.mkEnableOption "Enable dired-narrow.";
     posframe = lib.mkEnableOption "Enable dired-posframe.";
+    async = lib.mkEnableOption "Let dired move files asynchronously.";
   };
 
   config.programs.emacs.init.usePackage = lib.mkIf tools.dired.enable {
     dired = {
       enable = true;
-      hook = ["(dired-mode . (lambda () (dired-omit-mode) (hl-line-mode) (setq-local visible-cursor nil)))"];
+      hook = [
+        "(dired-mode . (lambda () (dired-omit-mode) (hl-line-mode) (setq-local visible-cursor nil)))"
+      ];
       setopt = {
-	dired-recursive-deletes = lib.mkDefault "'always";
+        dired-recursive-deletes = lib.mkDefault "'always";
         dired-listing-switches = lib.mkDefault ''"-agho --group-directories-first"'';
       };
       generalOne.global-leader."d" = lib.mkIf keybinds.leader-key.enable '''("dired" . dired)'';
@@ -25,32 +28,56 @@ in
 
     wdired = {
       enable = true;
-      generalTwoConfig.":n".dired-mode-map."w" = lib.mkIf keybinds.evil.enable (lib.mkDefault "'wdired-change-to-wdired-mode");
-      bindLocal.dired-mode-map."w" = lib.mkIf (!keybinds.evil.enable) (lib.mkDefault "wdired-change-to-wdired-mode");
+      generalTwoConfig.":n".dired-mode-map."w" = lib.mkIf keybinds.evil.enable (
+        lib.mkDefault "'wdired-change-to-wdired-mode"
+      );
+      bindLocal.dired-mode-map."w" = lib.mkIf (!keybinds.evil.enable) (
+        lib.mkDefault "wdired-change-to-wdired-mode"
+      );
     };
 
     diredfl = {
       enable = true;
-      hook = ["(dired-mode . diredfl-mode)"];
+      hook = [ "(dired-mode . diredfl-mode)" ];
     };
 
     dired-narrow = lib.mkIf tools.dired.narrow {
       enable = true;
-      generalTwo.":n".dired-mode-map."N" = lib.mkIf keybinds.evil.enable (lib.mkDefault "'dired-narrow-fuzzy");
-      bindLocal.dired-mode-map."N" = lib.mkIf (!keybinds.evil.enable) (lib.mkDefault "dired-narrow-fuzzy");
+      generalTwo.":n".dired-mode-map."N" = lib.mkIf keybinds.evil.enable (
+        lib.mkDefault "'dired-narrow-fuzzy"
+      );
+      bindLocal.dired-mode-map."N" = lib.mkIf (!keybinds.evil.enable) (
+        lib.mkDefault "dired-narrow-fuzzy"
+      );
     };
 
     dired-posframe = lib.mkIf tools.dired.posframe {
       enable = true;
-      generalTwo.":n".dired-mode-map."M-t" = lib.mkIf keybinds.evil.enable (lib.mkDefault "'dired-posframe-mode");
-      bindLocal.dired-mode-map."M-t" = lib.mkIf (!keybinds.evil.enable) (lib.mkDefault "dired-posframe-mode");
+      generalTwo.":n".dired-mode-map."M-t" = lib.mkIf keybinds.evil.enable (
+        lib.mkDefault "'dired-posframe-mode"
+      );
+      bindLocal.dired-mode-map."M-t" = lib.mkIf (!keybinds.evil.enable) (
+        lib.mkDefault "dired-posframe-mode"
+      );
     };
 
     dired-hide-dotfiles = {
       enable = true;
-      hook = ["(dired-mode . dired-hide-dotfiles-mode)"];
-      generalTwoConfig.":n".dired-mode-map."H" = lib.mkIf keybinds.evil.enable (lib.mkDefault "'dired-hide-dotfiles-mode");
-      bindLocal.dired-mode-map."H" = lib.mkIf (!keybinds.evil.enable) (lib.mkDefault "dired-hide-dotfiles-mode");
+      hook = [ "(dired-mode . dired-hide-dotfiles-mode)" ];
+      generalTwoConfig.":n".dired-mode-map."H" = lib.mkIf keybinds.evil.enable (
+        lib.mkDefault "'dired-hide-dotfiles-mode"
+      );
+      bindLocal.dired-mode-map."H" = lib.mkIf (!keybinds.evil.enable) (
+        lib.mkDefault "dired-hide-dotfiles-mode"
+      );
     };
-  } ;
+
+    async = lib.mkIf tools.dired.async {
+      enable = true;
+      config = ''
+        (autoload 'dired-async-mode "dired-async.el" nil t)
+        (dired-async-mode)
+      '';
+    };
+  };
 }
